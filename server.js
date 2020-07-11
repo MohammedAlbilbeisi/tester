e2f95b2c391300d189c9738da966fd524a0279a5
Mohammed Zeyad <mohammmedze.gmal.com>
2020-07-11 21:36:32 +0300

    test

---
+++ server.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express')
+const app = express()
+//const port = 3000
+const port = process.env.PORT || 3001;
+app.get('/', (req, res) => res.send('Hello World! ^_^'))
+
+
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+
+
+
+
